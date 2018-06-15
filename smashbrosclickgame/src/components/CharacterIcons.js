ffd5004473d6b4a3b44d83fbdc521d0a52c76d77
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-06-15 18:34:03 -0500

    Did lots of experimenting to try to get it to adjust the image's clicked value rather than the space's on the page.  No luck yet

--- smashbrosclickgame/src/components/CharacterIcons.js
+++ smashbrosclickgame/src/components/CharacterIcons.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+// Sets max height/width for icon images
 var iconStyle = {
     maxHeight: "200px",
     maxWidth: "200px"
-}
+  }
+
+
+// class CharacterIcons extends Component{
+// render(){
+//     return(
+//         <div className="container m-0 mw-100">
+//         <div className="row justify-content-center">
+
+//             {/* {console.log(props)} */}
+//             {/* Go through the array of icons to generate them on the page */}
+//             {icons.map(icon => (
+//                 <button type="button" onClick={props.increaseScore} >
+//                     <div className="m-2">
+//                         <img className="icon" id="false" src={icon.path} alt={icon.alt} style={iconStyle} />
+//                     </div>
+//                 </button>
+//             ))}
+
+
+//         </div>
+//     </div>
+//     )
+// }
+// }
 
 
 
@@ -13,15 +38,14 @@ const CharacterIcons = props => (
         <div className="row justify-content-center">
 
             {/* {console.log(props)} */}
+            {/* Go through the array of icons to generate them on the page */}
             {props.icons.map(icon => (
                 <button type="button" onClick={props.increaseScore} >
                     <div className="m-2">
-                        <img className="icon" id="false" src={icon.path} alt={icon.alt} style={iconStyle}/>
+                        <img className="icon" id={icon.clicked} data-num={icon.id} src={icon.path} alt={icon.alt} style={iconStyle} />
                     </div>
                 </button>
             ))}
-
-
         </div>
     </div>
 )
--- smashbrosclickgame/src/components/Header.js
+++ smashbrosclickgame/src/components/Header.js
@@ -1,101 +1,136 @@
 import React, { Component } from 'react';
 import CharacterIcons from './CharacterIcons';
 
+
+// Array of character icons
 let icons = [
   {
     id: 1,
     path: "./img/captain.png",
-    clicked: false,
+    clicked: "false",
     alt: "Captain Falcon"
   }, {
     id: 2,
     path: "./img/donkey.png",
-    clicked: false,
+    clicked: "false",
     alt: "Donkey Kong"
   }, {
     id: 3,
     path: "./img/falco.png",
-    clicked: false,
+    clicked: "false",
     alt: "Falco"
   },
   {
     id: 4,
     path: "./img/fox.png",
-    clicked: false,
+    clicked: "false",
     alt: "Fox McCloud"
   },
   {
     id: 5,
     path: "./img/kirby.png",
-    clicked: false,
+    clicked: "false",
     alt: "Kirby"
   },
   {
     id: 6,
     path: "./img/link.png",
-    clicked: false,
+    clicked: "false",
     alt: "Link"
   },
   {
     id: 7,
     path: "./img/luigi.png",
-    clicked: false,
+    clicked: "false",
     alt: "Luigi"
   },
   {
     id: 8,
     path: "./img/mario.png",
-    clicked: false,
+    clicked: "false",
     alt: "Mario"
   },
   {
     id: 9,
     path: "./img/ness.png",
-    clicked: false,
+    clicked: "false",
     alt: "Ness"
   },
   {
     id: 10,
     path: "./img/pikachu.png",
-    clicked: false,
+    clicked: "false",
     alt: "Pikachu"
   },
   {
     id: 11,
     path: "./img/samus.png",
-    clicked: false,
+    clicked: "false",
     alt: "Samus"
   },
   {
     id: 12,
     path: "./img/sonic.png",
-    clicked: false,
+    clicked: "false",
     alt: "Sonic"
   }];
 
 
-  
+// Shuffle the array after a button is clicked
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 
 
 
 
 class Header extends Component {
 
+  // Set initial score to zero
   state = {
     score: 0
   };
 
+  // Define function for increasing score.  Passes in the specific button clicked
   increaseScore = (event) => {
-    console.log(event.target.id)
+
+    // How does this automatically display the shuffle on the page?
+    // NOTE THIS DOESN'T WORK.  It shuffles the pictures but it makes it so only the region has been clicked NOT the image itself
+    // For some reason the images shuffle, but the true/false for clicked doesn't move with them
+    shuffle(icons);
+
+    // Set id to true if false (to see whether the icon has been clicked)
     if (event.target.id === "false") {
       event.target.id = "true"
-      this.setState({ score: this.state.score + 1 });
+      console.log(event.target)
+
+      // Increase score by 1
+      this.setState({ score: this.state.score + 1});
+
+      // Determine if the player has won the game
+      // TODO: Actually display a victory message on the page
+      if (this.state.score + 1 === 12) {
+        console.log("You Win!")
+      }
     }
-    else { console.log("You have clicked this already.  You lose.") }
+
+    // Display a losing message
+    else {
+      console.log("You have clicked this already.  You lose.");
+      this.setState( { score: 0 })
+      // return this.renderPage();
+    }
   };
 
-
-  render() {
+  // Did this to see if I could fix the clicked true/false issue but I'm not sure it's doing it
+  renderPage = () => {
     return (
       <div className="container m-0 mw-100">
 
@@ -108,8 +143,9 @@ class Header extends Component {
 
         <div className="justify-content-center">
           <h5 className="text-center">Click a character icon to earn a point!  However, don't click the same one twice or you lose!</h5>
-          </div>
+        </div>
 
+      
         <CharacterIcons
           icons={icons}
           count={this.state.score}
@@ -117,6 +153,10 @@ class Header extends Component {
         />
       </div>
     )
+  }
+
+  render() {
+    return this.renderPage()
   }
 
 };
