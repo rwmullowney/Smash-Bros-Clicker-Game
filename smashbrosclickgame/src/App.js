91d6e5971540176fbdbf204274da80c444427f69
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-06-14 22:59:13 -0500

    Displays all character icons on the page

--- smashbrosclickgame/src/App.js
+++ smashbrosclickgame/src/App.js
@@ -2,44 +2,93 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CharacterIcons from './components/CharacterIcons';
 
 
 let icons = [
-  {
-      id: 1,
-      link: "../../public/img/captain.png",
-      clicked: false,
-      alt: "Captain Falcon"
-  },    {
-      id: 2,
-      link: "../../public/img/donkey.png",
-      clicked: false,
-      alt: "Donkey Kong"
-  },    {
-      id: 3,
-      link: "../../public/img/falco.png",
-      clicked: false,
-      alt: "Falco"
-  },
-  {
-      id: 4,
-      link: "../../public/img/fox.png",
-      clicked: false,
-      alt: "Fox McCloud"
-  }];
+    {
+        id: 1,
+        path: "./img/captain.png",
+        clicked: false,
+        alt: "Captain Falcon"
+    }, {
+        id: 2,
+        path: "./img/donkey.png",
+        clicked: false,
+        alt: "Donkey Kong"
+    }, {
+        id: 3,
+        path: "./img/falco.png",
+        clicked: false,
+        alt: "Falco"
+    },
+    {
+        id: 4,
+        path: "./img/fox.png",
+        clicked: false,
+        alt: "Fox McCloud"
+    },
+    {
+        id: 5,
+        path: "./img/kirby.png",
+        clicked: false,
+        alt: "Kirby"
+    },
+    {
+        id: 6,
+        path: "./img/link.png",
+        clicked: false,
+        alt: "Link"
+    },
+    {
+        id: 7,
+        path: "./img/luigi.png",
+        clicked: false,
+        alt: "Luigi"
+    },
+    {
+        id: 8,
+        path: "./img/mario.png",
+        clicked: false,
+        alt: "Mario"
+    },
+    {
+        id: 9,
+        path: "./img/ness.png",
+        clicked: false,
+        alt: "Ness"
+    },
+    {
+        id: 10,
+        path: "./img/pikachu.png",
+        clicked: false,
+        alt: "Pikachu"
+    },
+    {
+        id: 11,
+        path: "./img/samus.png",
+        clicked: false,
+        alt: "Samus"
+    },
+    {
+        id: 12,
+        path: "./img/sonic.png",
+        clicked: false,
+        alt: "Sonic"
+    }];
 
 
 
 class App extends Component {
-  render() {
-    return (
+    render() {
+        return (
 
-<div>
-      <Header />
-      <CharacterIcons icons={icons} />
-      </div>
-    );
-  }
+            <div>
+                <Header />
+                <CharacterIcons icons={icons} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- smashbrosclickgame/src/components/CharacterIcons.js
+++ smashbrosclickgame/src/components/CharacterIcons.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 
 
-const CharacterIcons = () => (
+const CharacterIcons = props => (
     <div className="container m-0 mw-100">
         <div className="row justify-content-center">
 
-            {iconsArray.map()}
+            {console.log(props)}
+            {props.icons.map(icon => (
+                <div className="m-2">
+                    <img src={icon.path} alt={icon.alt}/>
+                </div>
+            ))}
+
+            {/* {props.icons.map(icon => (
+                <img src={item.path}>
+            ))} */}
 
 
         </div>
     </div>
 )
 
-export default Header
+export default CharacterIcons
