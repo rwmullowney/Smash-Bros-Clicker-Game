8a2673e51ff6e1c5189435a954ac75b26c66fb6c
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-06-16 09:33:03 -0500

    Prep for Github Pages deployment

--- smashbrosclickgame/src/components/CharacterIcons.js
+++ smashbrosclickgame/src/components/CharacterIcons.js
@@ -8,30 +8,6 @@ var iconStyle = {
   }
 
 
-// class CharacterIcons extends Component{
-// render(){
-//     return(
-//         <div className="container m-0 mw-100">
-//         <div className="row justify-content-center">
-
-//             {/* {console.log(props)} */}
-//             {/* Go through the array of icons to generate them on the page */}
-//             {icons.map(icon => (
-//                 <button type="button" onClick={props.increaseScore} >
-//                     <div className="m-2">
-//                         <img className="icon" id="false" src={icon.path} alt={icon.alt} style={iconStyle} />
-//                     </div>
-//                 </button>
-//             ))}
-
-
-//         </div>
-//     </div>
-//     )
-// }
-// }
-
-
 
 const CharacterIcons = props => (
     <div className="container m-0 mw-100">
